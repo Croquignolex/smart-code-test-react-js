345235e32934d1ec58d89f0136f45dfdd381493a
Alex Stéphane NGOMBOL <gpetitalex10@gmail.com>
2021-06-16 11:26:21 +0100

    Create home page rip

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import HomePage from "./pages/HomePage";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.StrictMode><HomePage /></React.StrictMode>,
   document.getElementById('root')
 );
 
